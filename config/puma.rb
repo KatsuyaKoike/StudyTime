f61094dcf681c7b1982ab557b1c80d48d1882415
Katsuya Koike <noonkk05@gmail.com>
2021-01-24 16:04:17 +0900

    [add] Install RuboCop
    ソースコードの可読性向上のため、Rubyの静的解析ツールを導入

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 # デフォルトの設定
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+max_threads_count = ENV.fetch('RAILS_MAX_THREADS', 5)
+min_threads_count = ENV.fetch('RAILS_MIN_THREADS') { max_threads_count }
 threads min_threads_count, max_threads_count
-port        ENV.fetch("PORT") { 3000 }
-environment ENV.fetch("RAILS_ENV") { "development" }
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+port        ENV.fetch('PORT', 3000)
+environment ENV.fetch('RAILS_ENV', 'development')
+pidfile ENV.fetch('PIDFILE', 'tmp/pids/server.pid')
 plugin :tmp_restart
 
 # ソケット通信用に追加
-app_root = File.expand_path("../..", __FILE__)
+app_root = File.expand_path('..', __dir__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
